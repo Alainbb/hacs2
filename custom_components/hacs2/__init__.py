d0f19b6d56576d89ce1e69deadecd7c4eedd09a3
Alainbb <alain.berinstain@gmail.com>
2025-11-29 12:14:57 -0500

    Update __init__.py

--- custom_components/hacs2/__init__.py
+++ custom_components/hacs2/__init__.py
@@ -1,1 +1,16 @@
+"""The HACS2 integration."""
 
+import logging
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
+LOGGER = logging.getLogger(__name__)
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up HACS2 from a config entry."""
+    LOGGER.info("HACS2 integration loaded")
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a config entry."""
+    return True
